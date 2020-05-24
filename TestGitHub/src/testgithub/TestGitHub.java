4fd397b2d785c7452b3aa62d31b718a0f3a7923a
lordeveloper <lor.developer@gmail.com>
2020-05-24 11:38:23 +0200

    Aggiornato println

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -15,7 +15,7 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ciao2");
+        System.out.println("Ciao22");
     }
     
 }
