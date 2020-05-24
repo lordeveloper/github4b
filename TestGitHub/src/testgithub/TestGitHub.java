a9e2cb6025bab28182e95cc74ce0cbd10e267268
Lorenzo <lor.developer@gmail.com>
2020-05-24 11:34:45 +0200

    Inserito println

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -15,7 +15,7 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ecco");
+        System.out.println("Ciao");
     }
     
 }
