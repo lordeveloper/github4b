11e4f7250c2fe8c328f61dac9759f44afcd1e6ac
Lorenzo <lor.developer@gmail.com>
2020-05-27 09:42:37 +0200

    Revert "Merge origin/master"
    This reverts commit 0da5df41aee31a2a77d01b692093a9e934842a8d.

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -15,7 +15,8 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ciao");
+        System.out.println("Ciao22");
+        System.out.println("Ciao23");
 
     }
     
