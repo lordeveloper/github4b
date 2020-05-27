0da5df41aee31a2a77d01b692093a9e934842a8d
Lorenzo <lor.developer@gmail.com>
2020-05-27 08:48:24 +0200

    Merge origin/master
    Conflicts:
    	TestGitHub/src/testgithub/TestGitHub.java

--- TestGitHub/src/testgithub/TestGitHub.java
+++ TestGitHub/src/testgithub/TestGitHub.java
@@ -15,8 +15,7 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ciao22");
-        System.out.println("Ciao23");
+        System.out.println("Ciao");
 
     }
     
